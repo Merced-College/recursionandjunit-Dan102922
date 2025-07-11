11e6a3b458c90df157889827b14d17abab6ee1c2
Dan102922 <motad8940@gmail.com>
2025-07-11 11:50:33 -0700

    Added stringClean

--- RecursiveImplementations.java
+++ RecursiveImplementations.java
@@ -9,7 +9,7 @@ public static int count8(int n) {
   // Checks the next digit
   int nextDigit = (n / 10) % 10;
 
-  // 1st case: The last digit is 8 and followed by another 8 (count 2)
+  // Case 1: The last digit is 8 and followed by another 8 (count 2)
   if (lastDigit == 8 && nextDigit == 8) {
     return 2 + count8(n / 10);
   }
@@ -31,7 +31,7 @@ public static int countHi(String str) {
     return 0;
 }
 
-  // 1st case: If first two characters are hi,
+  // Case 1: If first two characters are hi,
   if (str.substring(0, 2).equals("hi")) {
     return 1 + countHi(str.substring(2));
   }
@@ -48,7 +48,7 @@ public static int countHi2(String str) {
     return 0;
 }
 
-  // 1st case: If first two characters are hi, print, if it has an x before it, skip
+  // Case 1: If first two characters are hi, print, if it has an x before it, skip
   if (str.substring(0, 3).equals("xhi")) {
     return countHi2(str.substring(3));
 
@@ -68,7 +68,7 @@ public static int count(String str, String sub) {
     return 0;
 }
 
-  // 1st case: Match found, catch it and 
+  // Case 1: Match found, catch it and 
   if (str.startsWith(sub)) {
     return 1 + count(str.substring(sub.length()), sub) ;
   }
@@ -77,4 +77,20 @@ public static int count(String str, String sub) {
   else {
     return count(str.substring(1));
   }
+}
+
+
+public static String stringClean(String str) {
+  // The Base case: if there is less than 2 characters, it will just skip to the end
+  if (str.length() < 2) {
+    return str;
+}
+
+  // Case 1: If matching character(s) found, duplicate characters are cleaned up, so only one of each remains
+  if (str.charAt(0) == str.charAt(1)) {
+    return stringClean(str.substring(1));
+  }
+
+  // Case 2: if there is no match, move on to the next character
+  return str.charAt(0) + stringClean(str.substring(1));
 }
