38b56f12c0f4c9d6a3f5ec00351cf2b18b6a9721
Dan102922 <motad8940@gmail.com>
2025-07-11 09:48:52 -0700

    Fixed equal in countHi, and added countHi2

--- RecursiveImplementations.java
+++ RecursiveImplementations.java
@@ -32,12 +32,32 @@ public static int countHi(String str) {
 }
 
   // 1st case: If first two characters are hi,
-  if (str.substring(0, 2) == "hi") {
+  if (str.substring(0, 2).equals("hi")) {
     return 1 + countHi(str.substring(2));
   }
 
   // Case 2: Not hi, skips a character
   else {
     return countHi(str.substring(1));
+  }
+}
+
+public static int countHi2(String str) {
+  // The Base case: if there is only one character, it can't spell hi at all
+  if (str.length() < 2) {
+    return 0;
+}
+
+  // 1st case: If first two characters are hi, print, if it has an x before it, skip
+  if (str.substring(0, 3).equals("xhi")) {
+    return countHi2(str.substring(3));
+
+  } else if (str.substring(0, 2).equals("hi")) {
+    return 1 + countHi2(str.substring(2));
+  }
+
+  // Case 2: Not hi, skips a character
+  else {
+    return countHi2(str.substring(1));
   }
 }
